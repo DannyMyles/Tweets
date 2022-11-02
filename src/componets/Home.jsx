453c49c32e7eb8c95b13bb248fece10dbda0968c
Danny <dannymyles1999@gmail.com>
2022-11-02 15:13:06 +0300

    005: Fetch Api

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -1,12 +1,21 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 const Home = () => {
-  const [blogs, setblogs] = useState([
-    {title: 'Good Kenyan',body: 'Joining the blog', author: 'Tembu', id:1},
-    {title: 'Here',body: 'Joining the blog', author: 'James', id:2},
-    {title: 'Save me',body: 'Joining the blog', author: 'Danny', id:3},
-    {title: 'Actas',body: 'Joining the blog', author: 'Maina', id:4}
+  // APi
+  const url ='http://localhost:4200/blogs'
+  const [blogs, setblogs] = useState([null]);
 
-  ]);
+  //APi fetch
+  useEffect(() => {
+    fetch(url)
+    .then(res => {
+      return res.json()})
+      .then(data => {
+        setblogs(data)
+      })
+  }, []);
+
+
+
 
   return (
      <>
