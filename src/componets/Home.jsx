7bba11ca47f74a03566c62b7371ae414880cb116
Danny <dannymyles1999@gmail.com>
2022-11-03 09:41:02 +0300

    008: ErrorHandling

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -1,26 +1,42 @@
-
 import BlogList from './BlogList';
 import {useState,useEffect} from 'react'
+
 
 const Home = () => { 
   // APi
   const url ='http://localhost:4200/blogs'
   const [blogs, setblogs] = useState([]);
+  const  [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   //APi fetch
   useEffect(() => {
     fetch(url)
-    .then(res => {
+      .then(res => {
+        //Error handling
+        if (!res.ok) {
+          throw Error('Could not fetch blog list ')
+        }
       return res.json()})
       .then(data => {
         setblogs(data)
+        //If there is error setIsPending is turned to false so that it stops loading
+        setIsPending(false)
+        setError(null)
+
+      })
+      // Catch errror
+      .catch((err)=>{
+        alert(err.message)
       })
   }, []);
 
   return (
-     <>
-     <BlogList  blogs={blogs}/>
-     </>
+     <div className='home'>
+      {error && <div>{error}</div>}
+     { isPending && <div>Loading ... </div>}
+     {blogs && <BlogList  blogs={blogs}/>}
+     </div>
 
   )
 }
