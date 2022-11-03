6e677f0a4576511999d72f9801cc8873b37da13a
Danny <dannymyles1999@gmail.com>
2022-11-03 10:08:50 +0300

    008: Adds a custom hook

--- src/componets/Home.jsx
+++ src/componets/Home.jsx
@@ -1,36 +1,8 @@
 import BlogList from './BlogList';
-import {useState,useEffect} from 'react'
+import useFetch from './useFetch';
 
-
-const Home = () => { 
-  // APi
-  const url ='http://localhost:4200/blogs'
-  const [blogs, setblogs] = useState([]);
-  const  [error, setError] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-
-  //APi fetch
-  useEffect(() => {
-    fetch(url)
-      .then(res => {
-        //Error handling
-        if (!res.ok) {
-          throw Error('Could not fetch blog list ')
-        }
-      return res.json()})
-      .then(data => {
-        setblogs(data)
-        //If there is error setIsPending is turned to false so that it stops loading
-        setIsPending(false)
-        setError(null)
-
-      })
-      // Catch errror
-      .catch((err)=>{
-        alert(err.message)
-      })
-  }, []);
-
+const Home = () => {
+  const {data:blogs,error, isPending} = useFetch('http://localhost:4200/blogs');
   return (
      <div className='home'>
       {error && <div>{error}</div>}
